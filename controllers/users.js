d410ab0a21d84657cf1a80a6d73de2b9931d8538
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-14 01:31:27 +1000

    14.2

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,14 @@
 const bcrypt = require('bcryptjs');
 const jsonWebToken = require('jsonwebtoken');
-const User = require('../models/user')
+const User = require('../models/user');
+const {
+  ERROR_CODE_WRONG_DATA,
+  ERROR_CODE_NOT_FOUND,
+  ERROR_CODE_DEFAULT,
+} = require('../error');
 
-const ERROR_CODE_WRONG_DATA = 400;
-const ERROR_CODE_NOT_FOUND = 404;
-const ERROR_CODE_DEFAULT = 500;
 const ERROR_CODE_DUPLICATE = require('../error/duplicateError');
-const ERROR_CODE_AUTH = 401;
+const ERROR_CODE_AUTH = require('../error/authError');
 
 module.exports.getUsers = (req, res, next) => {
   User.find({})
@@ -18,34 +20,34 @@ module.exports.getUser = (req, res, next) => {
   User.findById(req.params.userId)
     .then((user) => {
       if (!user) {
-        throw new ERROR_CODE_NOT_FOUND('Error...');
-      } else {
-        next(res.send(user));
+        throw new ERROR_CODE_NOT_FOUND('User not found');
       }
+      res.send(user);
     })
     .catch((err) => {
       if (err.name === "CastError") {
-        next(new ERROR_CODE_WRONG_DATA(`Error...`));
+        next(new ERROR_CODE_WRONG_DATA('Invalid user ID'));
       }
       next(err);
     });
 };
 
-module.exports.createUser = (req, res, next) => {
-  const { name, about, avatar, email, password } = req.body;
-  bcrypt.hash(password, 10)
-    .then((hashedPassword) => User.create({ name, about, avatar, email, password: hashedPassword }))
-    .then((user) => res.send(user.toJSON()))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        next(new ERROR_CODE_WRONG_DATA(`Error...`));
-      } else if (err.code === 11000) {
-        next(new ERROR_CODE_DUPLICATE(`Error...`));
-      }
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const { name, about, avatar, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, about, avatar, email, password: hashedPassword });
+    res.send(user.toJSON());
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      next(new ERROR_CODE_WRONG_DATA('Invalid user data'));
+    } else if (err.code === 11000) {
+      next(new ERROR_CODE_DUPLICATE('User already exists'));
+    } else {
       next(err);
     }
-    );
-}
+  }
+};
 
 module.exports.updateUserProfile = (req, res, next) => {
   const { name, about } = req.body;
