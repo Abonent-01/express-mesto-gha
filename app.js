30594ce372d202a211c4792bf5deae4540339ec6
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-13 17:10:34 +1000

    14.2

--- app.js
+++ app.js
@@ -1,27 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const router = require('./routes/router');
-
-
-const ERROR_CODE_NOT_FOUND = 404;
-
-
-const auth = require('./middlewares/auth');
-const { createUser, login } = require('./middlewares/auth');
-const { validateCreateUser, validateLogin } = require('./middlewares/validate');
-
 
 const { PORT = 3000 } = process.env; // Слушаем 3000 порт
 
 const app = express();
 app.use(express.json());
-app.post('/signin', validateLogin, login);
-app.post('/signup', validateCreateUser, createUser);
-app.use(auth);
-app.use(router);
+app.use(cookieParser());
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(router);
 
 app.listen(PORT, () => {
     // Если всё работает, консоль покажет, какой порт приложение слушает
--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const { createUser, login } = require('../controllers/users');
+const auth = require('../middlewares/auth');
+const { validateCreateUser, validateLogin } = require('../middleware/validate');
+const ERROR_CODE_NOT_FOUND = 404;
 
+router.post('/signup', validateCreateUser, createUser);
+router.post('/signin', validateLogin, login);
+router.use(auth);
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
 router.use('*', (req, res, next) => {
-  next(new Erorr('Error...'))
+  next(new ERROR_CODE_NOT_FOUND('Error...'))
 });
 
 module.exports = router;
