88ecd3fd06ea0207a5d77165203fe7c23ab51797
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-13 17:27:56 +1000

    14.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,23 @@
 const jwt = require('jsonwebtoken');
+const ERROR_CODE_AUTH = 401;
 
 const auth = (req, res, next) => {
   let token;
   try {
     token = req.cookies.jwt;
   } catch (err) {
-    throw new Error('Authentication error: No JWT found in cookies.');
+    throw new ERROR_CODE_AUTH('Error...');
   }
-
   let payload;
+
   try {
     payload = jwt.verify(token, 'SECRET');
   } catch (err) {
-    throw new Error('Authentication error: Invalid JWT.');
+    throw new ERROR_CODE_AUTH('Error...');
   }
 
   req.user = payload;
   next();
-};
+}
 
 module.exports = auth;
