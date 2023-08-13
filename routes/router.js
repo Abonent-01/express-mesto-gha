3b81dd7ca9f6ca9ed543f5d163a0099ad3ffe5f4
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-13 15:51:30 +1000

    14.2

--- routes/router.js
+++ routes/router.js
@@ -2,16 +2,11 @@ const router = require('express').Router();
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
 
-
-
-const ERROR_CODE_NOT_FOUND = 404;
-
-
 router.use('/users', usersRouter);
 router.use('/cards', cardsRouter);
 
 router.use('*', (req, res, next) => {
-  next(new ERROR_CODE_NOT_FOUND('Error...'))
+  next(new Erorr('Error...'))
 });
 
 module.exports = router;
