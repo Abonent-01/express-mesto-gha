db1f2d362fe128f08eb8a43a2fa2383569fcd0ca
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-15 01:35:48 +1000

    14.2

--- controllers/cards.js
+++ controllers/cards.js
@@ -11,67 +11,71 @@ module.exports.getCards = (req, res, next) => {
 };
 
 module.exports.createCard = (req, res, next) => {
-  const { _id } = req.user;
   const { name, link } = req.body;
-  Card.create({ name, link, owner: _id })
+  const owner = req.user._id;
+  Card.create({ name, link, owner })
     .then((card) => res.send(card))
     .catch((err) => {
-      if (err.name = "ValidationError") {
-        next(new ERROR_CODE_WRONG_DATA(`ValidationError`));
+      if (err.name === 'ValidationError') {
+        next(new ERROR_CODE_WRONG_DATA('Некорректные данные при создании карточки'));
       } else {
         next(err);
       }
     });
-}
-
+};
 
 module.exports.deleteCard = (req, res, next) => {
-  Card.findByIdAndRemove(req.params.cardId)
-    .orFail(() => new ERROR_CODE_NOT_FOUND(`Error...`))
+  const removeCard = () => {
+    Card.findByIdAndRemove(req.params.cardId)
+      .then((card) => res.send(card))
+      .catch(next);
+  };
+
+  Card.findById(req.params.cardId)
     .then((card) => {
-      if (card.owner.toString() === req.user._id) {
-        card.deleteOne(card)
-          .then((cards) => res.send(cards))
-          .catch(next)
-      } else {
-        throw new ERROR_CODE_FORBIDDEN('Чужую карточку удалить нельзя')
+      if (!card) next(new ERROR_CODE_NOT_FOUND('Карточки не существует'));
+      if (req.user._id === card.owner.toString()) {
+        return removeCard();
       }
+      return next(new ERROR_CODE_FORBIDDEN('Попытка удалить чужую карточку'));
     })
     .catch(next);
 };
 
 module.exports.likeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
     .then((card) => {
       if (!card) {
-        throw new ERROR_CODE_NOT_FOUND('Error...');
-      } else {
-        next(res.send(card));
+        return next(
+          new ERROR_CODE_NOT_FOUND(
+            'Запрашиваемая карточка для добавления лайка не найдена',
+          ),
+        );
       }
+      return res.send(card);
     })
-    .catch((err) =>{
-      if (err.name === "CastError") {
-        next(new ERROR_CODE_WRONG_DATA('Error...'))
-      } else {
-        next(err);
-      }
-    })
+    .catch(next);
 };
 
 module.exports.dislikeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Card not found' });
-      } else {
-        res.send(card);
+        return next(
+          new ERROR_CODE_NOT_FOUND(
+            'Запрашиваемая карточка для удаления лайка не найдена',
+          ),
+        );
       }
+      return res.send(card);
     })
-    .catch((err) => {
-      if (err.name === "CastError") {
-        next(new ERROR_CODE_WRONG_DATA(`Error...`));
-      } else {
-        next(err);
-      }
-    });
+    .catch(next);
 };
