087aacf72d0618a32af45ecee738a754d342a5ed
Aleksandr <a.p.soldatenko@yandex.ru.com>
2023-08-16 00:44:05 +1000

    14.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,22 +2,22 @@ const jwt = require('jsonwebtoken');
 const ERROR_CODE_AUTH = require('../error/authError');
 
 const auth = (req, res, next) => {
-  let token;
-  try {
-    token = req.cookies.jwt;
-  } catch (err) {
-    throw new ERROR_CODE_AUTH('Error...');
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    return next(new ERROR_CODE_AUTH('Error...'));
   }
+
   let payload;
 
   try {
     payload = jwt.verify(token, 'SECRET');
   } catch (err) {
-    throw new ERROR_CODE_AUTH('Error...');
+    return next(new ERROR_CODE_AUTH('Error...'));
   }
 
   req.user = payload;
   next();
-}
+};
 
 module.exports = auth;
